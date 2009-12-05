c11eb8dc306c9d7a183e2aa31ad4b5edd619a388
William Melody <wmelody@gmail.com>
2009-12-05 17:24:52 -0600

    making REE use .deb on ubuntu

--- ruby-enterprise/attributes/default.rb
+++ ruby-enterprise/attributes/default.rb
@@ -1,8 +1,20 @@
 ruby_enterprise_edition Mash.new unless attribute?("ruby_enterprise_edition")
-ruby_enterprise_edition[:version] = '1.8.7-20090928' unless ruby_enterprise_edition.has_key?(:version)
-ruby_enterprise_edition[:install_path] = "/opt/ruby-enterprise-#{ruby_enterprise_edition[:version]}" unless ruby_enterprise_edition.has_key?(:install_path)
-ruby_enterprise_edition[:url] = "http://rubyforge.org/frs/download.php/64475/ruby-enterprise-#{ruby_enterprise_edition[:version]}.tar.gz"
-ruby_enterprise_edition[:cow_friendly] = %Q[#{ruby_enterprise_edition[:install_path]}/bin/ruby -e "exit 1 unless GC.respond_to?(:copy_on_write_friendly=)"]
+
+case platform
+when "ubuntu"
+  ruby_enterprise_edition[:version] = '1.8.7-2009.10_i386'
+  ruby_enterprise_edition[:package_name] = "ruby-enterprise_#{ruby_enterprise_edition[:version]}.deb"
+  ruby_enterprise_edition[:install_path] = "/usr/local"
+  
+  # note: the id after download.php is dependent on the package
+  ruby_enterprise_edition[:url] = "http://rubyforge.org/frs/download.php/66164/#{ruby_enterprise_edition[:package_name]}"
+else
+  ruby_enterprise_edition[:version] = '1.8.7-20090928' unless ruby_enterprise_edition.has_key?(:version)
+  ruby_enterprise_edition[:install_path] = "/opt/ruby-enterprise-#{ruby_enterprise_edition[:version]}" unless ruby_enterprise_edition.has_key?(:install_path)
+  # note: the id after download.php is dependent on the package
+  ruby_enterprise_edition[:url] = "http://rubyforge.org/frs/download.php/64475/ruby-enterprise-#{ruby_enterprise_edition[:version]}.tar.gz"
+  ruby_enterprise_edition[:cow_friendly] = %Q[#{ruby_enterprise_edition[:install_path]}/bin/ruby -e "exit 1 unless GC.respond_to?(:copy_on_write_friendly=)"]
+end
 
 languages[:ruby][:gems_dir] = "#{ruby_enterprise_edition[:install_path]}/lib/ruby/gems/1.8"
 languages[:ruby][:ruby_bin] = "#{ruby_enterprise_edition[:install_path]}/bin/ruby"
--- ruby-enterprise/recipes/default.rb
+++ ruby-enterprise/recipes/default.rb
@@ -23,58 +23,90 @@
 ##
  
 # TODO: A future release of Chef should have a source_package resource to handle installs like this
- 
-%w(zlib1g-dev libssl-dev libreadline5-dev).each do |p|
-  package p
-end
 
-remote_file "/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}.tar.gz" do
-  source node[:ruby_enterprise_edition][:url]
-  not_if { ::File.exist?("/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}.tar.gz") }
-end
- 
-execute "Expand ruby enterprise edition tarball" do
-  cwd "/usr/local/src"
-  command "tar xzf ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}.tar.gz"
-  not_if { ::File.directory?("/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}") }
-end
- 
-execute "Install ruby enterprise edition" do
-  cwd "/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}"
-  command "./installer --auto=#{node[:ruby_enterprise_edition][:install_path]} && echo '#{node[:ruby_enterprise_edition][:version]}' > /etc/ruby-enterprise-version"
-  not_if {
-    ::File.exists?('/etc/ruby-enterprise-version') &&
-    ::File.read('/etc/ruby-enterprise-version').chomp == node[:ruby_enterprise_edition][:version] && 
-    ::File.exist?("#{node[:ruby_enterprise_edition][:install_path]}/bin/ruby") && 
-    system(node[:ruby_enterprise_edition][:cow_friendly])
-  }
-end
- 
-%w(ruby rake irb gem).each do |cmd|
-  link "/usr/bin/#{cmd}" do
-    to "#{node[:ruby_enterprise_edition][:install_path]}/bin/#{cmd}"
+case node[:platform]
+when "ubuntu"
+  remote_file "/usr/local/src/#{node[:ruby_enterprise_edition][:package_name]}" do
+    source node[:ruby_enterprise_edition][:url]
+    not_if { ::File.exist?("/usr/local/src/#{node[:ruby_enterprise_edition][:package_name]}") }
   end
-end
-
-execute "update rubygems" do
-  command "gem update --system"
-end
-
-%W[ ohai chef ].each do |g|
-  gem_package g do
-    source "http://gems.opscode.com"
-    action :install
+  
+  execute "install #{node[:ruby_enterprise_edition][:package_name]}" do
+    cwd "/usr/local/src"
+    command "dpkg -i #{node[:ruby_enterprise_edition][:package_name]}"
+    not_if { ::File.exists?("/usr/local/bin/ruby") }
   end
-end
-
-File.open('/etc/environment', 'r+') do |f|
-  lines = f.readlines
-  lines.each do |l|
-    unless l.match('ruby-enterprise')
-      l.gsub!(%r{PATH=\"/usr/local}, "PATH=\"#{node[:ruby_enterprise_edition][:install_path]}/bin:/usr/local")
+  
+  %w(ruby rake irb gem).each do |cmd|
+    link "/usr/bin/#{cmd}" do
+      to "#{node[:ruby_enterprise_edition][:install_path]}/bin/#{cmd}"
     end
   end
-  f.pos = 0
-  f.print lines
-  f.truncate(f.pos)
+  
+  execute "update rubygems" do
+    command "gem update --system"
+  end
+
+  %W[ ohai chef ].each do |g|
+    gem_package g do
+      source "http://gems.opscode.com"
+      action :install
+    end
+  end
+else
+  
+  %w(zlib1g-dev libssl-dev libreadline5-dev).each do |p|
+    package p
+  end
+  
+  remote_file "/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}.tar.gz" do
+    source node[:ruby_enterprise_edition][:url]
+    not_if { ::File.exist?("/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}.tar.gz") }
+  end
+   
+  execute "Expand ruby enterprise edition tarball" do
+    cwd "/usr/local/src"
+    command "tar xzf ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}.tar.gz"
+    not_if { ::File.directory?("/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}") }
+  end
+   
+  execute "Install ruby enterprise edition" do
+    cwd "/usr/local/src/ruby-enterprise-#{node[:ruby_enterprise_edition][:version]}"
+    command "./installer --auto=#{node[:ruby_enterprise_edition][:install_path]} && echo '#{node[:ruby_enterprise_edition][:version]}' > /etc/ruby-enterprise-version"
+    not_if {
+      ::File.exists?('/etc/ruby-enterprise-version') &&
+      ::File.read('/etc/ruby-enterprise-version').chomp == node[:ruby_enterprise_edition][:version] && 
+      ::File.exist?("#{node[:ruby_enterprise_edition][:install_path]}/bin/ruby") && 
+      system(node[:ruby_enterprise_edition][:cow_friendly])
+    }
+  end
+   
+  %w(ruby rake irb gem).each do |cmd|
+    link "/usr/bin/#{cmd}" do
+      to "#{node[:ruby_enterprise_edition][:install_path]}/bin/#{cmd}"
+    end
+  end
+  
+  execute "update rubygems" do
+    command "gem update --system"
+  end
+  
+  %W[ ohai chef ].each do |g|
+    gem_package g do
+      source "http://gems.opscode.com"
+      action :install
+    end
+  end
+  
+  File.open('/etc/environment', 'r+') do |f|
+    lines = f.readlines
+    lines.each do |l|
+      unless l.match('ruby-enterprise')
+        l.gsub!(%r{PATH=\"/usr/local}, "PATH=\"#{node[:ruby_enterprise_edition][:install_path]}/bin:/usr/local")
+      end
+    end
+    f.pos = 0
+    f.print lines
+    f.truncate(f.pos)
+  end
 end

90c1b3ab28795057fcc8152226d2c7033dc09605
William Melody <wmelody@gmail.com>
2009-12-09 15:19:17 -0600

    setting only the astrails-safe attributes we need

--- astrails-safe/attributes/default.rb
+++ astrails-safe/attributes/default.rb
@@ -3,11 +3,6 @@
 # or
 # set[:astrails_safe][:pgp][:key] = "some@key"
 
-set[:astrails_safe] = {
-  :local => {
-    :path => '/var/backups/astrails-safe/:kind/:id'
-  }, :keep => {
-    :local => 20,
-    :s3 => 30
-  }
-}
+set[:astrails_safe][:local][:path]  = '/var/backups/astrails-safe/:kind/:id'
+set[:astrails_safe][:keep][:local]  = 20
+set[:astrails_safe][:keep][:s3]     = 30

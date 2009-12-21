c92c70cf2177a6d51dada024f370f799f09e908c
William Melody <wmelody@gmail.com>
2009-12-21 16:31:31 -0600

    beginning sphinx cookbook

---
+++ sphinx/attributes/default.rb
@@ -0,0 +1,2 @@
+set[:sphinx][:version] = "0.9.9"
+set[:sphinx][:url] = "http://www.sphinxsearch.com/downloads/sphinx-#{sphinx[:version]}.tar.gz"
---
+++ sphinx/recipes/default.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: sphinx
+# Recipe:: default
+#
+# Copyright 2009, Kyakia LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+case node[:platform]
+when "ubuntu"
+  remote_file "/usr/local/src/sphinx-#{node[:sphinx][:version]}.tar.gz" do
+    source node[:sphinx][:url]
+    not_if { ::File.exist?("/usr/local/src/sphinx-#{node[:sphinx][:version]}.tar.gz") }
+  end
+  
+  execute "Expand sphinx tarball" do
+    cwd "/usr/local/src"
+    command "tar xzf sphinx-#{node[:sphinx][:version]}.tar.gz"
+    not_if { ::File.directory?("/usr/local/src/sphinx-#{node[:sphinx][:version]}") }
+  end
+  
+  execute "Make and install sphinx" do
+    cwd "/usr/local/src/sphinx-#{node[:sphinx][:version]}"
+    command "./configure --prefix=/usr/local && make && sudo make install"
+    not_id { ::File.exist?("/usr/local/bin/indexer") }
+  end
+end
